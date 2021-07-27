1d7c912b1d2c56b847d2c406c084e87aa7da1cda
Leandro Suarez Vece <leovece1@gmail.com>
2021-07-27 06:02:17 -0300

    commit

---
+++ LectorDeVoz/Lector de voz/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Lector_de_voz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnPausa = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.btnAcercaDe = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.cbolistar = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackVolumen = new System.Windows.Forms.TrackBar();
+            this.trackVelocidad = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.trackVolumen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackVelocidad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(677, 448);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(105, 36);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnPausa
+            // 
+            this.btnPausa.Location = new System.Drawing.Point(788, 448);
+            this.btnPausa.Name = "btnPausa";
+            this.btnPausa.Size = new System.Drawing.Size(105, 36);
+            this.btnPausa.TabIndex = 2;
+            this.btnPausa.Text = "Pausa";
+            this.btnPausa.UseVisualStyleBackColor = true;
+            this.btnPausa.Click += new System.EventHandler(this.btnPausa_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Location = new System.Drawing.Point(899, 448);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(105, 36);
+            this.btnDetener.TabIndex = 3;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // btnAcercaDe
+            // 
+            this.btnAcercaDe.Location = new System.Drawing.Point(899, 496);
+            this.btnAcercaDe.Name = "btnAcercaDe";
+            this.btnAcercaDe.Size = new System.Drawing.Size(105, 36);
+            this.btnAcercaDe.TabIndex = 6;
+            this.btnAcercaDe.Text = "Abrir";
+            this.btnAcercaDe.UseVisualStyleBackColor = true;
+            this.btnAcercaDe.Click += new System.EventHandler(this.btnAcercaDe_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(788, 496);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(105, 36);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(677, 496);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(105, 36);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // cbolistar
+            // 
+            this.cbolistar.FormattingEnabled = true;
+            this.cbolistar.Location = new System.Drawing.Point(24, 489);
+            this.cbolistar.Name = "cbolistar";
+            this.cbolistar.Size = new System.Drawing.Size(187, 24);
+            this.cbolistar.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 457);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Seleccionar Voz";
+            // 
+            // trackVolumen
+            // 
+            this.trackVolumen.Location = new System.Drawing.Point(372, 441);
+            this.trackVolumen.Maximum = 100;
+            this.trackVolumen.Name = "trackVolumen";
+            this.trackVolumen.Size = new System.Drawing.Size(221, 56);
+            this.trackVolumen.TabIndex = 9;
+            this.trackVolumen.TickFrequency = 20;
+            this.trackVolumen.Value = 50;
+            // 
+            // trackVelocidad
+            // 
+            this.trackVelocidad.Location = new System.Drawing.Point(372, 476);
+            this.trackVelocidad.Minimum = -10;
+            this.trackVelocidad.Name = "trackVelocidad";
+            this.trackVelocidad.Size = new System.Drawing.Size(221, 56);
+            this.trackVelocidad.TabIndex = 10;
+            this.trackVelocidad.TickFrequency = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(267, 441);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Volumen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(267, 481);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "velocidad";
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(0, 0);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(1016, 349);
+            this.axAcroPDF1.TabIndex = 13;
+            this.axAcroPDF1.Enter += new System.EventHandler(this.axAcroPDF1_Enter);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 544);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackVelocidad);
+            this.Controls.Add(this.trackVolumen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbolistar);
+            this.Controls.Add(this.btnAcercaDe);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnPausa);
+            this.Controls.Add(this.btnLeer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackVolumen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackVelocidad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnPausa;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Button btnAcercaDe;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ComboBox cbolistar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackVolumen;
+        private System.Windows.Forms.TrackBar trackVelocidad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ LectorDeVoz/Lector de voz/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+namespace Lector_de_voz
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer voz;
+        List<VoiceInfo> listarvoces = new List<VoiceInfo>();
+        List<string> texto = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            voz = new SpeechSynthesizer();
+
+            foreach (InstalledVoice x in voz.GetInstalledVoices())
+            {
+                listarvoces.Add(x.VoiceInfo);
+                cbolistar.Items.Add(x.VoiceInfo.Name);
+            }
+
+            cbolistar.SelectedIndex = 0;
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            voz = new SpeechSynthesizer();
+
+            int ind;
+            double volumen = trackVolumen.Value;
+            double velocidad = trackVelocidad.Value;
+
+            ind = cbolistar.SelectedIndex;
+            String nombrevoces = listarvoces.ElementAt(ind).Name;
+            voz.SelectVoice(nombrevoces);
+
+            voz.Volume = (int)volumen;
+            voz.Rate = (int)velocidad;
+            foreach(string a in texto)
+            {
+                voz.SpeakAsync(a);
+            }
+
+            btnPausa.Text = "Pausa";
+            
+        }
+
+        private void btnPausa_Click(object sender, EventArgs e)
+        {
+            if (voz != null)
+            {
+
+                if (voz.State == SynthesizerState.Speaking)
+                {
+                    voz.Pause(); btnPausa.Text = "Resumen";
+                }
+                else if (voz.State == SynthesizerState.Paused)
+                {
+                    voz.Resume(); btnPausa.Text = "Pausar";
+                }
+
+            }
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            if (voz != null)
+            {
+                if (voz.State == SynthesizerState.Paused)
+                {
+                    voz.Resume(); btnPausa.Text = "Pausar";
+                }
+                voz.Dispose();
+
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                voz = new SpeechSynthesizer();
+                using (SaveFileDialog vai = new SaveFileDialog())
+                {
+
+                    vai.Filter = "Wav files|*.wav";
+                    if (vai.ShowDialog() == DialogResult.OK)
+                    {
+                        int ind;
+                        double volumen = trackVolumen.Value;
+                        double velocidad = trackVelocidad.Value;
+
+                        ind = cbolistar.SelectedIndex;
+                        String nombrevoces = listarvoces.ElementAt(ind).Name;
+                        voz.SelectVoice(nombrevoces);
+
+                        FileStream team = new FileStream(vai.FileName, FileMode.Create, FileAccess.Write);
+                        voz.SetOutputToWaveStream(team);
+                        foreach (string a in texto)
+                        {
+                            voz.SpeakAsync(a);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Intentar de nuevo", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAcercaDe_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            axAcroPDF1.src = openFileDialog1.FileName;
+            leerPdf(axAcroPDF1.src);
+        }
+
+        private void axAcroPDF1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leerPdf(string directorio)
+        {
+            int i = 0;
+            var pdf = new PdfDocument(new PdfReader(directorio));
+            var strategy = new LocationTextExtractionStrategy();
+            
+
+            for(i=1; i<= pdf.GetNumberOfPages(); i++)
+            {
+                var page = pdf.GetPage(i);
+                texto.Add(PdfTextExtractor.GetTextFromPage(page));
+            }
+        }
+    }
+}
